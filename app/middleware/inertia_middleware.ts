46dcc7588078f03da3306f6f9ebeff55be638fc7
Harminder Virk <virk.officials@gmail.com>
2025-11-04 16:45:11 +0530

    fix: ctx properties access within the Inertia middleware

--- app/middleware/inertia_middleware.ts
+++ app/middleware/inertia_middleware.ts
@@ -5,28 +5,43 @@ import BaseInertiaMiddleware from '@adonisjs/inertia/inertia_middleware'
 
 export default class InertiaMiddleware extends BaseInertiaMiddleware {
   share(ctx: HttpContext) {
-    const errorsBag = ctx.session.flashMessages.get('errorsBag', {})
+    /**
+     * The share method is called everytime an Inertia page is rendered. In
+     * certain cases, a page may get rendered before the session middleware
+     * or the auth middleware are executed. For example: During a 404 request.
+     *
+     * In that case, we must always assume that HttpContext is not fully hydrated
+     * with all the properties
+     */
+    const { session, auth } = ctx as Partial<HttpContext>
+
+    /**
+     * Fetching the first error from the flash messages
+     */
+    const errorsBag = session?.flashMessages.get('errorsBag') ?? {}
     const error: string | undefined = Object.keys(errorsBag)
-      .filter((code) => {
-        return code !== 'E_VALIDATION_ERROR'
-      })
+      .filter((code) => code !== 'E_VALIDATION_ERROR')
       .map((code) => errorsBag[code])[0]
 
+    /**
+     * Data shared with all Inertia pages. Make sure you are using
+     * transformers for rich data-types like Models.
+     */
     return {
       errors: ctx.inertia.always(this.getValidationErrors(ctx)),
       flash: ctx.inertia.always({
         error: error,
       }),
-      user: ctx.inertia.always(
-        ctx.auth.user ? UserTransformer.transform(ctx.auth.user) : undefined
-      ),
+      user: ctx.inertia.always(auth?.user ? UserTransformer.transform(auth.user) : undefined),
     }
   }
 
   async handle(ctx: HttpContext, next: NextFn) {
     await this.init(ctx)
+
     const output = await next()
     this.dispose(ctx)
+
     return output
   }
 }
